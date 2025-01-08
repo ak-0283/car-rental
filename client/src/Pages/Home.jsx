fa77cac2af08509945388cab5804b37f900e1b29
abhaykumar-0285 <abhay.kr2803@gmail.com>
2025-01-08 19:06:23 +0530

    updating scroll button using react

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Booking from "../components/Booking/Booking";
 import "./home.css";
 import Hero from "../components/Hero/Hero";
 import Work from "../components/Working/Work";
 import Footer from "../components/Footer/Footer";
+
 const Home = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <main>
       <div className="hero">
@@ -17,7 +43,15 @@ const Home = () => {
         <Work />
       </div>
       <Footer />
-    
+
+      {/* Scroll to Top Button */}
+      <button
+        id="scrollButton"
+        onClick={scrollToTop}
+        style={{ display: isVisible ? "block" : "none" }}
+      >
+        <i className="fa-solid fa-arrow-up fa-lg"></i>
+      </button>
     </main>
   );
 };
